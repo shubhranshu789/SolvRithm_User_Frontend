60f5a8060e143de4514dd7023c0c06106eb912a4
shubhranshu789 <66314743+shubhranshu789@users.noreply.github.com>
2025-09-21 13:02:05 +0530

    Initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import { Analytics } from '@vercel/analytics/next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'Solvrith',
+  title: 'SOLVRITHM',
   description: '',
   generator: '',
   icons: {
